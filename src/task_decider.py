c9caabc1abbf6517de1f4de90b34c13ac12e8935
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-07 13:50:00 +0100

    adds second compoarison

--- src/task_decider.py
+++ src/task_decider.py
@@ -5,3 +5,7 @@ def task_decider_function(task1, task2):
         if task1.description == "Cook Dinner" or task2.description == "Cook Dinner":
             return "Wash the Dishes"
     
+    if task1.description == "Wash the Dishes" or task2.description == "Wash the Dishes":
+        if task1.description == "Clean Windows" or task2.description == "Clean Windows":
+            return "Clean Windows"
+    
--- tests/test_task_decider.py
+++ tests/test_task_decider.py
@@ -7,6 +7,10 @@ class TestTaskDecider(unittest.TestCase):
     def setUp(self):
         self.task1 = Task("Wash the Dishes", 15)
         self.task2 = Task("Cook Dinner", 60)
+        self.task3 = Task("Clean Windows", 90)
 
     def test_first_comparison(self):
         self.assertEqual("Wash the Dishes", task_decider_function(self.task1, self.task2))
+
+    def test_second_comparison(self):
+        self.assertEqual("Clean Windows", task_decider_function(self.task1, self.task3))
