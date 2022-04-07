ea1831c206ad0496ee2a4e3715da35c3f05449ea
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-07 15:24:23 +0100

    completes the extension

--- src/task_decider.py
+++ src/task_decider.py
@@ -1,20 +1,5 @@
-# def task_decider_function(task1, task2):
-#     tasks_list = ["Wash the Dishes", "Cook Dinner", "Clean Windows"]
-
-#     if task1.description == tasks_list[0] or task2.description == tasks_list[0]:
-#         if task1.description == tasks_list[1] or task2.description == tasks_list[1]:
-#             return "Wash the Dishes"
-    
-#     if task1.description == tasks_list[0] or task2.description == tasks_list[0]:
-#         if task1.description == tasks_list[2] or task2.description == tasks_list[2]:
-#             return "Clean Windows"
-    
-#     if task1.description == tasks_list[1] or task2.description == tasks_list[1]:
-#         if task1.description == tasks_list[2] or task2.description == tasks_list[2]:
-#             return "Cook Dinner"
-
 def task_decider_function(task1, task2):
-    tasks_list = ["Wash the Dishes", "Cook Dinner", "Clean Windows", ""]
+    tasks_list = ["Wash the Dishes", "Cook Dinner", "Do Ironing", "Wash Clothes", "Clean Windows"]
     index_list = []
     for index, task in enumerate(tasks_list):
         if task1.description == task:
@@ -23,7 +8,7 @@ def task_decider_function(task1, task2):
         if task2.description == task:
             index_list.append(index)
     
-    sorted(index_list)
+    index_list = sorted(index_list)
     difference = index_list[1] - index_list[0]
     if difference % 2 == 0:
         return tasks_list[index_list[1]]
--- tests/test_task_decider.py
+++ tests/test_task_decider.py
@@ -7,16 +7,42 @@ class TestTaskDecider(unittest.TestCase):
     def setUp(self):
         self.task1 = Task("Wash the Dishes", 15)
         self.task2 = Task("Cook Dinner", 60)
-        self.task3 = Task("Clean Windows", 90)
+        self.task3 = Task("Do Ironing", 15)
+        self.task4 = Task("Wash Clothes", 60)
+        self.task5 = Task("Clean Windows", 90)
 
     def test_first_comparison(self):
         self.assertEqual("Wash the Dishes", task_decider_function(self.task1, self.task2))
 
     def test_second_comparison(self):
-        self.assertEqual("Clean Windows", task_decider_function(self.task1, self.task3))
+        self.assertEqual("Do Ironing", task_decider_function(self.task1, self.task3))
 
     def test_third_comparison(self):
         self.assertEqual("Cook Dinner", task_decider_function(self.task2, self.task3))
 
     def test_fourth_comparison(self):
-        self.assertEqual("Wash the Dishes", task_decider_function(self.task3, self.task1))
+        self.assertEqual("Do Ironing", task_decider_function(self.task3, self.task4))
+
+    def test_fifth_comparison(self):
+        self.assertEqual("Do Ironing", task_decider_function(self.task4, self.task3))
+
+    def test_sixth_comparison(self):
+        self.assertEqual("Wash Clothes", task_decider_function(self.task5, self.task4))
+    
+    def test_seventh_comparison(self):
+        self.assertEqual("Clean Windows", task_decider_function(self.task5, self.task1))
+
+    def test_eighth_comparison(self):
+        self.assertEqual("Clean Windows", task_decider_function(self.task1, self.task5))
+    
+    def test_nineth_comparison(self):
+        self.assertEqual("Clean Windows", task_decider_function(self.task3, self.task5))
+    
+    def test_tenth_comparison(self):
+        self.assertEqual("Do Ironing", task_decider_function(self.task1, self.task3))
+
+    def test_eleventh_comparison(self):
+        self.assertEqual("Cook Dinner", task_decider_function(self.task2, self.task5))
+
+    def test_twelveth_comparison(self):
+        self.assertEqual("Cook Dinner", task_decider_function(self.task5, self.task2))
