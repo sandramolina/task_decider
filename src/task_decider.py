18bb1ccb390a9c38eb057b242171dd813e6ed339
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-07 13:46:13 +0100

    improves the code for tghe decider

--- src/task_decider.py
+++ src/task_decider.py
@@ -1,8 +1,7 @@
 
 def task_decider_function(task1, task2):
-    # preferred_task = task1.description
-    if task1.description == "Wash the Dishes" and task2.description == "Cook Dinner":
-        return task1.description
 
-    if task1.description == "Cook Dinner" and task2.description == "Wash the Dishes":
-        return task2.description
+    if task1.description == "Wash the Dishes" or task2.description == "Wash the Dishes":
+        if task1.description == "Cook Dinner" or task2.description == "Cook Dinner":
+            return "Wash the Dishes"
+    
--- tests/test_task_decider.py
+++ tests/test_task_decider.py
@@ -5,8 +5,8 @@ import unittest
 
 class TestTaskDecider(unittest.TestCase):
     def setUp(self):
-        self.task2 = Task("Wash the Dishes", 15)
-        self.task1 = Task("Cook Dinner", 60)
+        self.task1 = Task("Wash the Dishes", 15)
+        self.task2 = Task("Cook Dinner", 60)
 
     def test_first_comparison(self):
         self.assertEqual("Wash the Dishes", task_decider_function(self.task1, self.task2))
